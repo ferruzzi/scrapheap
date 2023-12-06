aeed2696c5a9fc18777cfdfc71ce57e83e40658e
ferruzzi <ferruzzi@amazon.com>
2023-12-06 14:28:43 -0800

    my full changes

--- projects/Douglas Mentoring/twilio_api_practice.py
+++ projects/Douglas Mentoring/twilio_api_practice.py
@@ -1,40 +1,51 @@
+import os
+import sys
+from datetime import date, datetime, timedelta
+
+import requests
+
 from twilio.rest import Client
-import requests, os
-from datetime import datetime, timedelta
+
+# Globals
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla"
 
 
-def get_stock_data():
-    # Pull data from API
+def key_not_found(key_name: str) -> None:
+    print(f"{key_name} not found.  Aborting...")
+    sys.exit()
 
-    global STOCK
-    STOCK = "TSLA"
-    alphavantage_api_key = os.environ["stock_key"]
-
-    parameters = {
-        "function": "TIME_SERIES_DAILY",
-        "symbol": STOCK,
-        "apikey": alphavantage_api_key
-    }
-    response = requests.get("https://www.alphavantage.co/query", params=parameters)
+def get_stock_data() -> dict[str, str]:
+    """Pull data from API."""
+    response = requests.get(
+        "https://www.alphavantage.co/query",
+        params={
+            "function": "TIME_SERIES_DAILY",
+            "symbol": STOCK,
+            "apikey": os.environ.get("stock_key", key_not_found("Alphavantage API Key")),
+        }
+    )
     return response.json()
 
 
-def get_stock_values(stock_data, yesterday, day_b_yesterday):
-    # Get the value from CLOSED
-    # check if its 5% lower or higher
-    # if no variance of 5% no more actions required
+def get_stock_values(stock_data: dict[str, str], yesterday_date: date, day_b_yesterday: date) -> bool:
+    """
+    Get the value from CLOSED
+    check if its 5% lower or higher
+    if no variance of 5% no more actions required
+    """
 
-    yesterday_close = float(stock_data["Time Series (Daily)"][f'{yesterday}']["4. close"])
-    day_b_yesterday_close = float(stock_data["Time Series (Daily)"][f'{day_b_yesterday}']["4. close"])
+    yesterday_close = float(stock_data["Time Series (Daily)"][str(yesterday_date)]["4. close"])
+    day_b_yesterday_close = float(stock_data["Time Series (Daily)"][str(day_b_yesterday)]["4. close"])
     print(f"##{yesterday_close}")
     print(f"##{day_b_yesterday_close}")
 
-    five_percent_less = float(day_b_yesterday_close) * 0.95
-    five_percent_higher = float(day_b_yesterday_close) * 1.05
+    five_percent_less = day_b_yesterday_close * 0.95
+    five_percent_higher = day_b_yesterday_close * 1.05
     print(f"**{five_percent_less}")
     print(f"**{five_percent_higher}")
 
-    if yesterday_close >= five_percent_less or yesterday_close <= five_percent_higher:
+    if five_percent_less <= yesterday_close <= five_percent_higher:
         print('Get news')
         return True
     else:
@@ -42,17 +53,19 @@ def get_stock_values(stock_data, yesterday, day_b_yesterday):
         return False
 
 
-def calculate_dates():
-    # Here we'll check the dates
-    # Expect to get update only from last 2 days
-    # If Monday or Sunday no updates required
-    week_day = datetime.now().date().weekday()
-    today_date = datetime.now().date()
+def calculate_dates() -> tuple[date | None, date | None]:
+    """
+    Here we'll check the dates
+    Expect to get update only from last 2 days
+    If Monday or Sunday no updates required
+    """
+    week_day: int = datetime.now().date().weekday()
+    today_date: date = datetime.now().date()
 
-    yesterday = None
-    day_b_yesterday = None
+    yesterday: date | None = None
+    day_b_yesterday: date | None = None
 
-    if week_day == 0 or week_day == 6:
+    if week_day in [0, 6]:
         print('No data')
     elif week_day == 1:
         yesterday = today_date - timedelta(days=1)
@@ -60,48 +73,43 @@ def calculate_dates():
     else:
         yesterday = today_date - timedelta(days=1)
         day_b_yesterday = today_date - timedelta(days=2)
+
     return yesterday, day_b_yesterday
 
 
-def get_news(date1, date2):
-    global COMPANY_NAME
+def get_news(start_date: date, end_date: date) -> list[dict[str, str]] | None:
+    top_headlines = requests.get(
+        "https://newsapi.org/v2/everything",
+        params={
+            "q": COMPANY_NAME,
+            "apiKey": os.environ.get("news_key", key_not_found("News API key")),
+            "language": "en",
+            "from": start_date,
+            "to": end_date,
+            "sort_by": "relevancy",
+            # "country": "us"
+        }
+    )
+    headlines = top_headlines.json()
 
-    news_api_key = os.environ["news_key"]
-    parameters = {
-        "q": COMPANY_NAME,
-        "apiKey": news_api_key,
-        "language": "en",
-        "from": date1,
-        "to": date2,
-        "sort_by": "relevancy",
-        # "country": "us"
-
-    }
     try:
-        top_headlines = requests.get("https://newsapi.org/v2/everything", params=parameters)
-
-        headlines = top_headlines.json()
-        selected_articles = []
-        for article in headlines['articles'][:3]:
-            selected_articles.append({
-                "title": article['title'],
-                "description": article['description']
-            })
-        return selected_articles
-    except:
+        return [
+            {"title": article["title"], "description": article["description"]}
+            for article in headlines["articles"][:3]
+        ]
+    except IndexError:
         return None
 
 
-def send_text(selected_articles):
-    global STOCK
-    account_sid = os.environ["sms_sid"]
-    auth_token = os.environ["sms_auth"]
+def send_text(articles: list[dict[str, str]] | None) -> None:
+    account_sid = os.environ.get("sms_sid", key_not_found("SMS SID"))
+    auth_token = os.environ.get("sms_auth", key_not_found("SMS token"))
 
-    if selected_articles:
-        for article in selected_articles:
+    if articles:
+        for article in articles:
             client = Client(account_sid, auth_token)
             message = client.messages.create(
-                body=f"{STOCK}💲💲💲 5%\nTitle: " + article["title"] + "\nDescription: " + article["description"],
+                body=f"{STOCK}💲💲💲 5%\nTitle: {article['title']}\nDescription: {article['description']}",
                 from="+13436553073",
                 to="+18259771913",
             )
@@ -122,22 +130,15 @@ def send_text(selected_articles):
 ## STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
-STOCK = "TSLA"
-COMPANY_NAME = "Tesla"
-
 # check date
-date1, date2 = calculate_dates()
-if date1 and date2:
+yesterday, previous_day = calculate_dates()
+if yesterday and previous_day:
     # Get stock data from API
     data = get_stock_data()
-    # Sort socks data
-    is_five_percent = get_stock_values(data, date1, date2)
-    # Check if there is a variance
-    if is_five_percent:
-        # Get news from API
-        selected_articles = get_news(date1, date2)
-        # Send text by API
-        send_text(selected_articles)
+    # Sort data and check if there is a variance
+    if get_stock_values(data, yesterday, previous_day):
+        # If there is, fetch news and send via SMS
+        send_text(get_news(yesterday, previous_day))
         print("text sent")
 else:
     print("nothing to update")
